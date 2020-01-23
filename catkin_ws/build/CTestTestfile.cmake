ca55c1580b77b3c0be87f3147f749c47f021a3ba
RakshitaaGM <rakshitaagm@gmail.com>
2020-01-23 13:37:38 -0500

    Catkin worksspace

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/rakshitaagm/git_ws/AuE893Spring20_RakshitaaGM/catkin_ws/src
+# Build directory: /home/rakshitaagm/git_ws/AuE893Spring20_RakshitaaGM/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/rakshitaagm/git_ws/AuE893Spring20_RakshitaaGM/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/rakshitaagm/git_ws/AuE893Spring20_RakshitaaGM/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/rakshitaagm/git_ws/AuE893Spring20_RakshitaaGM/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/rakshitaagm/git_ws/AuE893Spring20_RakshitaaGM/catkin_ws/src:$ROS_PACKAGE_PATH"
